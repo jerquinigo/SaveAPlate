d80c4baed00986c5ccd410091af2ba510ea162d1
Andrew KIl <kil.andrew@gmail.com>
2019-04-09 15:44:16 -0400

    refactor favorites queries & favorites route

--- backend/db/queries/favoriteQueries.js
+++ backend/db/queries/favoriteQueries.js
@@ -1,79 +1,77 @@
-const {db} = require('../index.js')
+const { db } = require("../index.js");
 
-
-getAllFavorites = (req,res,next) => {
-  db.any("SELECT clients.id AS clientId, clients.email, clients.name, clients.address_field AS clientAddress, clients.client_certificate AS client_cert, vendors.id AS vendorId, vendors.email, vendors.name AS vendor_name, vendors.address_field AS vendor_address, vendors.body AS vendor_description, vendors.telephone_number, employee_id_number AS ein FROM favorites JOIN clients ON clients.id=favorites.client_id JOIN vendors ON vendors.id=favorites.vendor_id")
-  .then(favorites => {
-    res.status(200).json({
-      status: "success",
-      favorites: favorites,
-      message: "recieved all favorites"
+getAllClientFavorites = (req, res, next) => {
+  db.any(
+    "SELECT DISTINCT users.id AS client_id, users.email, users.password_digest, users.type, users.address_field AS address, users.telephone_number AS telephone, users.client_certificate AS certificate FROM favorites JOIN users ON favorites.client_id = users.id ORDER BY client_id"
+  )
+    .then(favorites => {
+      res.status(200).json({
+        status: "success",
+        favorites: favorites,
+        message: "received all favorites"
+      });
     })
-  })
-  .catch(err => {
-    return next(err)
-  })
-}
+    .catch(err => {
+      return next(err);
+    });
+};
 
-getAllFavoritesById = (req,res,next) => {
-  let favoritesId = parseInt(req.params.id)
-  db.any("SELECT clients.id AS clientId, clients.email, clients.name, clients.address_field AS clientAddress, clients.client_certificate AS client_cert, vendors.id AS vendorId, vendors.email, vendors.name AS vendor_name, vendors.address_field AS vendor_address, vendors.body AS vendor_description, vendors.telephone_number, employee_id_number AS ein FROM favorites JOIN clients ON clients.id=favorites.client_id JOIN vendors ON vendors.id=favorites.vendor_id WHERE favorites.vendor_id=$1", [favoritesId])
-  .then(favorites => {
-    res.status(200).json({
-      status: "success",
-      favorites: favorites,
-      message: "recieved all favorites by id"
+getAllFavoritesById = (req, res, next) => {
+  const favoritesId = parseInt(req.params.id);
+  db.any(
+    "SELECT DISTINCT users.id AS client_id, users.email, users.password_digest, users.type, users.address_field AS address, users.telephone_number AS telephone, users.client_certificate AS certificate FROM favorites JOIN users ON favorites.client_id = users.id WHERE favorites.client_id = $1 ORDER BY client_id",
+    [favoritesId]
+  )
+    .then(favorites => {
+      res.status(200).json({
+        status: "success",
+        favorites: favorites,
+        message: "received all favorites by id"
+      });
     })
-  })
-  .catch(err => {
-    return next(err)
-  })
-}
+    .catch(err => {
+      return next(err);
+    });
+};
 
-createFavorite = (req,res,next) => {
-  db.one("INSERT INTO favorites(client_id, vendor_id) VALUES(${client_id}, ${vendor_id}) RETURNING vendor_id",
-  {
-    client_id: req.body.client_id,
-    vendor_id: req.body.vendor_id
-  }
-)
-.then(favorite => {
-  res.status(200).json({
-    status: "success",
-    favorite:favorite,
-    message: "created favorite"
-  })
-})
-.catch(err => {
-  return next(err)
-})
-}
+createFavorite = (req, res, next) => {
+  db.one(
+    "INSERT INTO favorites(client_id, vendor_id) VALUES(${client_id}, ${vendor_id}) RETURNING vendor_id",
+    {
+      client_id: req.body.client_id,
+      vendor_id: req.body.vendor_id
+    }
+  )
+    .then(favorite => {
+      res.status(200).json({
+        status: "success",
+        favorite: favorite,
+        message: "created favorite"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+};
 
-
-
-deleteFavorite = (req,res,next) => {
-  let favoriteId = parseInt(req.params.id)
+deleteFavorite = (req, res, next) => {
+  const favoriteId = parseInt(req.params.id);
   db.result("DELETE FROM favorites WHERE id=$1", favoriteId)
-  .then(result => {
-    res.status(200).json({
-      status: "success",
-      message: "removed a favorite",
-      result: result
+    .then(result => {
+      res.status(200).json({
+        status: "success",
+        message: "removed a favorite",
+        result: result
+      });
     })
-  })
-  .catch(err => {
-    return next(err)
-  })
-}
-
-
-
-
-
+    .catch(err => {
+      return next(err);
+    });
+};
 
 module.exports = {
-  getAllFavorites,
+  getAllClientFavorites,
   getAllFavoritesById,
   createFavorite,
   deleteFavorite
-}
+};
