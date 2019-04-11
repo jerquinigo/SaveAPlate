a35c68d279c0d8b58a4f908994799c1a084ae4f2
Wynter Reid <wynterreid@pursuit.org>
2019-04-11 14:56:30 -0400

    Demo logins are  functioning and on the landing page

--- frontend/src/containers/DemoLoginContainer.js
+++ frontend/src/containers/DemoLoginContainer.js
@@ -1,20 +1,20 @@
-// import DemoLogin from "../components/auth/DemoLogin.js";
-// import { connect } from "react-redux";
-// import { checkAuthenticateStatus } from "../actions/AuthActions.js";
-//
-// const mapStateToProps = state => {
-//   return {
-//     currentUser: state.currentUser
-//   };
-// };
-//
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     checkAuthenticateStatus: () => dispatch(checkAuthenticateStatus())
-//   };
-// };
-//
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(DemoLogin);
+import DemoLogin from "../components/auth/DemoLogin.js";
+import { connect } from "react-redux";
+import { checkAuthenticateStatus } from "../actions/AuthActions.js";
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    checkAuthenticateStatus: () => dispatch(checkAuthenticateStatus())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DemoLogin);
