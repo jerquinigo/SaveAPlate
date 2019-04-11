aea3d4045fe09a7786f92a1166455e9185033b65
Wynter Reid <wynterreid@pursuit.org>
2019-04-11 13:26:09 -0400

    change of files and imports

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -2,9 +2,9 @@ import { combineReducers } from "redux";
 import UserReducer from "./UserReducer.js";
 import AuthReducer from "./AuthReducer.js";
 
-const rootReducer = combineReducers({
+const RootReducer = combineReducers({
   users: UserReducer,
   auth: AuthReducer
 });
 
-export default rootReducer;
+export default RootReducer;
