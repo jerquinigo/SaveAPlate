417a27cfd212df039187c005da289c70fc5580cc
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-04-15 14:29:45 -0400

    added clients page added and some redux for clients

--- frontend/src/actions/ActionTypes.js
+++ frontend/src/actions/ActionTypes.js
@@ -3,3 +3,8 @@ export const RECEIVE_USER = "RECEIVE_USER";
 
 //users action type
 export const RECEIVED_ALL_USERS = "RECEIVED_ALL_USERS";
+
+
+//client actions
+
+export const POST_CLIENTS_CHANGES = "POST_CLIENTS_CHANGES"
--- frontend/src/actions/ClientActions.js
+++ frontend/src/actions/ClientActions.js
@@ -1,2 +1,20 @@
 // import * as clientsApi from "../utils/UtilUsers.js";
 // import { RECEIVED_ALL_USERS } from "./ActionTypes.js";
+import * as usersApi from '../utils/UtilUsers.js'
+
+import {POST_CLIENTS_CHANGES} from './ActionTypes.js'
+
+
+export const postClientChanges = (clientChange) => {
+  return {
+  type: POST_CLIENTS_CHANGES,
+  clientChange: clientChange
+  }
+}
+
+
+export const postClientFormChanges = () => dispatch => {
+  return
+
+
+}
