9f6a700f86ee69bfc7d550080411163ad6478917
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-04-15 12:42:07 -0400

    created clientProfile to display name and address. set up form in a child component and still need to create a post request after data has been entered

--- frontend/src/components/profiles/ClientProfile.js
+++ frontend/src/components/profiles/ClientProfile.js
@@ -1,17 +1,41 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import ClientProfileEditForm from "./ClientProfileEditForm.js";
 
+class ClientProfile extends Component {
+  state = {
+    editProfileButton: false,
+    name: "",
+    email: "",
+    address_field: "",
+    body: "",
+    telephone_number: "",
+    client_certificate: ""
+  };
 
-class ClientProfile extends Component{
+  componentDidMount() {
+    this.displayClientProfile();
+  }
 
-
-
-  render(){
-    console.log(this.props);
-    return(
-      <div className="clientProfileWrapper profile">
-      this is the clients page
+  displayClientProfile = () => {
+    return (
+      <div className="displayInfo">
+        <p>{this.props.currentUser.name}</p>
+        <p>{this.props.currentUser.address_field}</p>
       </div>
-    )
+    );
+  };
+
+
+
+  render() {
+    console.log(this.state, "in the webpage");
+    return (
+      <div className="clientProfileWrapper profile">
+        this is the clients page
+        {this.displayClientProfile()}
+        <ClientProfileEditForm />
+      </div>
+    );
   }
 }
 
---
+++ frontend/src/components/profiles/ClientProfileEditForm.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react'
+
+
+class ClientProfileEditForm extends Component{
+  constructor(){
+    super()
+    this.state = {
+      editProfileButton: false,
+      name: "",
+      email: "",
+      address_field: "",
+      body: "",
+      telephone_number: "",
+      client_certificate: "",
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  toggleEditFormLogic = () => {
+    this.setState({
+      editProfileButton: !this.state.editProfileButton
+    })
+  }
+
+  editProfile = () => {
+    return(
+      <button onClick={this.toggleEditFormLogic}>Edit Profile</button>
+    )
+  }
+
+  displayEditForm = () => {
+    return(
+      <div className="displayEditFormPage">
+        <form>
+
+          <input onChange={this.handleChange} name="name" type="text" placeholder="enter name"/>
+          <br / >
+          <input onChange={this.handleChange} name="email" type="text" placeholder="enter email" />
+          <br />
+          <input onChange={this.handleChange} name="address_field" type="text" placeholder="enter address" />
+          <br />
+          <input onChange={this.handleChange} name="body" type="text" placeholder="enter new description" />
+          <br />
+          <input onChange={this.handleChange} name="telephone_number" type="text" placeholder="enter telephone number" />
+          <br />
+
+          <input onChange={this.handleChange} name="client_certificate" type="text" placeholder="enter client certificate" />
+          <br />
+        </form>
+
+      </div>
+    )
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  render(){
+    return(
+      <div className="ClientFormMainPage">
+      {this.editProfile()}
+      {this.state.editProfileButton? this.displayEditForm(): null}
+      </div>
+    )
+  }
+}
+
+export default ClientProfileEditForm
