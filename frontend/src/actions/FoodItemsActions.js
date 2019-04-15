c07e34548355ff522161aeb5329b38b38cfd8e0b
Wynter Reid <wynterreid@pursuit.org>
2019-04-15 14:57:31 -0400

    removed unused action type

--- frontend/src/actions/FoodItemsActions.js
+++ frontend/src/actions/FoodItemsActions.js
@@ -1,6 +1,6 @@
 import {getFoodItemsByVendor} from '../utils/UtilFoodItems.js'
 import axios from "axios";
-import {RECEIVE_FOOD_ITEMS_BY_VENDOR, CREATE_FOOD_ITEM} from "./ActionTypes.js";
+import {RECEIVE_FOOD_ITEMS_BY_VENDOR} from "./ActionTypes.js";
 import * as foodItemsApi from "../utils/UtilFoodItems.js"
 
 export const receiveFoodItems = fooditems => {
