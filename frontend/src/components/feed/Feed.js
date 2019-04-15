70d2f04d80225a44386cd85c8fd78b2f794fd1bf
Andrew KIl <kil.andrew@gmail.com>
2019-04-15 12:58:41 -0400

    Refactor foodItemsQueries, add getFoodItemsByVendor, add claim status
    getFoodItemsByVendor: change id to vendor_id, comment out getFoodItemsForClient and all relevant pieces, replace with getFoodItemsForVendor, add ternary logic to button to render claimed or unclaimed

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -4,21 +4,43 @@ import axios from "axios";
 
 export default class Feed extends Component {
   state = {
-    foodItems: []
+    foodItems: [],
+    // clientID: 1,
+    vendorID: 1
   };
 
   componentDidMount() {
-    this.getFoodItemsForClient();
+    // this.getFoodItemsForClient();
+    this.getFoodItemsForVendor();
   }
 
-  getFoodItemsForClient = () => {
-    axios.get("/api/fooditems/client/1").then(foodItems => {
-      this.setState({
-        foodItems: foodItems.data.food_items
+  //   getFoodItemsForClient = () => {
+  //     axios
+  //       .get(`/api/fooditems/client/${this.state.clientID}`)
+  //       .then(foodItems => {
+  //         this.setState({
+  //           foodItems: foodItems.data.food_items
+  //         });
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       });
+  //   };
+  //   // at the moment, this function will only display foodItems for client #1 since "1" is hard-coded. will be updated
+
+  getFoodItemsForVendor = () => {
+    axios
+      .get(`/api/fooditems/vendor/${this.state.vendorID}`)
+      .then(foodItems => {
+        this.setState({
+          foodItems: foodItems.data.food_items
+        });
+      })
+      .catch(err => {
+        console.log(err);
       });
-    });
   };
-  // at the moment, this function will only display foodItems for client #1 since "1" is hard-coded. will be updated
+  // at the moment, this function will only display foodItems for vendor #1 since "1" is hard-coded. will be updated
 
   render() {
     console.log(this.state.foodItems);
@@ -31,7 +53,7 @@ export default class Feed extends Component {
             {this.state.foodItems.map((food, i) => {
               return (
                 <ul key={i}>
-                  <h3>Item: {i + 1}</h3>
+                  <h3>Item #: {i + 1}</h3>
                   <li>
                     <strong>Name: </strong>
                     {food.name}
@@ -42,6 +64,10 @@ export default class Feed extends Component {
                   </li>
                   <li>
                     <strong>Pick-up Time: </strong> {food.set_time}
+                  </li>
+                  <li>
+                    <button>{food.is_claimed ? "Claimed" : "Unclaimed"}</button>
+                    {/* this button will not actually do anything, for now, except display the claim status */}
                   </li>
                 </ul>
               );
