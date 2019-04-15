62a5e8cf819be39f8b4543a3b2469ece1d31f480
Andrew KIl <kil.andrew@gmail.com>
2019-04-15 13:47:15 -0400

    Add link to Feed component in client profile page

--- frontend/src/components/profiles/ClientProfile.js
+++ frontend/src/components/profiles/ClientProfile.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class ClientProfile extends Component {
   render() {
     console.log(this.props);
-    return <div className="clientProfileWrapper profile">CLIENTS page!</div>;
+    return (
+      <>
+        <div className="clientProfileWrapper profile">CLIENTS page!</div>
+        <NavLink to="/feed">Feed</NavLink>
+      </>
+    );
   }
 }
 
