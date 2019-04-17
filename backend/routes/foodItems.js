0b96a3f8b1d47a555d11b01fa36b5abc9f43ff06
Andrew KIl <kil.andrew@gmail.com>
2019-04-17 15:24:38 -0400

    added food items route revised

--- backend/routes/foodItems.js
+++ backend/routes/foodItems.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const {
   getAllFoodItems,
-  getClaimedFoodItemsByClient,
+  getAllClaimedFoodItems,
   getFoodItemsByClient,
   getFoodItemsByVendor,
   foodItemClaimStatus,
@@ -12,13 +12,12 @@ const {
 } = require("../db/queries/foodItemsQueries.js");
 
 router.get("/", getAllFoodItems);
+router.get("/client", getAllClaimedFoodItems);
 router.get("/clientpage/:name", getFoodItemsByClient);
-router.get("/client/:name", getClaimedFoodItemsByClient);
 router.get("/vendor/:id", getFoodItemsByVendor);
 router.patch("/claimstatus/:id", foodItemClaimStatus);
 router.post("/", createNewFoodItem);
 router.patch("/:id", updateFoodItem);
-router.patch("/claimstatus/:id", foodItemClaimStatus);
 router.delete("/:id", deleteFoodItem);
 
 module.exports = router;
