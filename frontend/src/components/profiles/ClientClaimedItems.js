3e87e322b1d9c6fca1a1e25387a2e1afa9f9bf40
Andrew KIl <kil.andrew@gmail.com>
2019-04-17 15:55:26 -0400

    fix

--- frontend/src/components/profiles/ClientClaimedItems.js
+++ frontend/src/components/profiles/ClientClaimedItems.js
@@ -1,52 +1,47 @@
-import React, {Component} from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-
-class ClientClaimedItems extends Component{
-  constructor(){
-    super()
+class ClientClaimedItems extends Component {
+  constructor() {
+    super();
     this.state = {
       claimedFoodItems: ""
-    }
+    };
   }
 
-  componentDidMount(){
-    this.getClaimedFoodItemsByClient('clienttester');
+  componentDidMount() {
+    this.getClaimedFoodItemsByClient();
   }
 
-getClaimedFoodItemsByClient = (name) => {
-  axios.get(`/api/fooditems/client/${name}`).then(res => {
-    this.setState({
-      claimedFoodItems: res.data.food_items
-    })
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
+  getClaimedFoodItemsByClient = () => {
+    axios
+      .get("/api/fooditems/client/")
+      .then(res => {
+        this.setState({
+          claimedFoodItems: res.data.food_items
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-displayClaimedFoodItems = () => {
-  this.state.claimedFoodItems.map(foodItem => {
-    return (
-      <>
-        <div>Address{foodItem.address_field}</div>
-        <div>Telephone{foodItem.telephone_number}</div>
-        <div>Telephone{foodItem.telephone_number}</div>
-      </>
-    )
+  displayClaimedFoodItems = () => {
+    this.state.claimedFoodItems.map(foodItem => {
+      return (
+        <>
+          <div>Address{foodItem.address_field}</div>
+          <div>Telephone{foodItem.telephone_number}</div>
+          <div>Telephone{foodItem.telephone_number}</div>
+        </>
+      );
+    });
+  };
 
-  })
-}
-
-
-  render(){
+  render() {
     console.log(this.state);
-    return(
-      <div className="clientClaimedItemsPage">
-
-      </div>
-    )
+    return <div className="clientClaimedItemsPage" />;
   }
 }
 
-export default ClientClaimedItems
+export default ClientClaimedItems;
