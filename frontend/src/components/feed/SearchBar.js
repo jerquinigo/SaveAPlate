c30cff701c98c61420c855ca52b3014b1ecacdaa
Wynter Reid <wynterreid@pursuit.org>
2019-04-18 17:52:06 -0400

    searchBar working for vendor name and food item

--- frontend/src/components/feed/SearchBar.js
+++ frontend/src/components/feed/SearchBar.js
@@ -2,64 +2,23 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 
 
-class SearchBar extends Component {
-  this.state = {
-    textInput: ""
-  }
+const SearchBar = (props) => {
 
-  handleChange = e => {
-    this.setState({
-      textInput: e.target.value
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-
-  }
-
-  handleChange = (e) => {
-      let searchResult = this.state.allFoodItems.filter(item => {
-        return (item.vendor_name.toLowerCase() === this.state.textInput.toLowerCase() || item.name.toLowerCase() === this.state.textInput.toLowerCase() )})
-
-      this.setState({
-        textInput: e.target.value,
-        userSearchResults: searchResult
-      })
-    }
-
-    handleSubmit = (e) => {
-    e.preventDefault()
-    let searchResult = this.state.allSongs.filter(song => {
-      let theSong = song.title.toLowerCase()
-
-      let text = this.state.textInput.toLowerCase()
-
-      return theSong.includes(text)
-    })
-      this.setState({
-        userSearchResults: searchResult, textInput: "",
-        submitted: true
-      })
-
-    }
-
-  render(){
     return (
-        <form onSubmit = {this.handleSubmit}>
+      <>
+        <form onSubmit = {props.handleSubmit}>
           <input
             type='text'
-            onChange={thishandleChange}
+            onChange={props.handleChange}
             placeholder=''
-            value={this.state.textInput}
+            value={props.textInput}
           />
           <button type='submit'>Submit</button>
         </form>
-
-      </div>
+      </>
     )
   }
-}
 
 
-export default Searchbar
+
+export default SearchBar
