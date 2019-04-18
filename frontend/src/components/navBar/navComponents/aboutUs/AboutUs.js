bbd547d1902a8a2a53ece82ec70ef49db9e8e81d
Andrew KIl <kil.andrew@gmail.com>
2019-04-18 13:04:15 -0400

    Add team to about us

--- frontend/src/components/navBar/navComponents/aboutUs/AboutUs.js
+++ frontend/src/components/navBar/navComponents/aboutUs/AboutUs.js
@@ -9,11 +9,6 @@ export default function About() {
       <br />
       <br />
       <div className="about-text">
-        Save a Plate was formed in 2019 by four Full Stack Web Developers with
-        the goal of helping the food rescue movement in NYC.
-      </div>
-      <br />
-      <div className="about-text">
         <h2>Our Mission</h2>
         <div>
           At Save a Plate, our mission is to reduce food waste in New York City.
@@ -22,6 +17,57 @@ export default function About() {
           SNAP and learning about food/hunger through statistics and articles.
         </div>
       </div>
+      <br />
+      <div className="about-text">
+        Save a Plate was formed in 2019 by four Full Stack Web Developers with
+        the goal of helping the food rescue movement in NYC.
+      </div>
+      <div>
+        <h2>Our Team</h2>
+        <img
+          src="https://avatars3.githubusercontent.com/u/43793502?s=400&v=4
+https://avatars3.githubusercontent.com/u/43793502?s=400&v=4
+"
+          width="100"
+          alt="wynter"
+        />
+        <br />
+        <a href="https://github.com/wynterreid">Wynter Reid</a>
+        <br />
+        <br />
+        <img
+          src="https://avatars2.githubusercontent.com/u/43793569?s=460&v=4
+https://avatars2.githubusercontent.com/u/43793569?s=460&v=4
+"
+          width="100"
+          alt="jonathan"
+        />
+        <br />
+        <a href="https://github.com/jerquinigo">Jonathan Erquinigo</a>
+        <br />
+        <br />
+        <img
+          src="https://avatars1.githubusercontent.com/u/41707636?s=460&v=4
+https://avatars1.githubusercontent.com/u/41707636?s=460&v=4
+"
+          width="100"
+          alt="deyvi"
+        />
+        <br />
+        <a href="https://github.com/SurgamSurgam">Deyvi Ortiz</a>
+        <br />
+        <br />
+        <img
+          src="https://avatars1.githubusercontent.com/u/22200791?s=460&v=4
+https://avatars1.githubusercontent.com/u/22200791?s=460&v=4
+"
+          width="100"
+          alt="andrew"
+        />
+        <br />
+        <a href="https://github.com/Andrew-Kil">Andrew Kil</a>
+      </div>
+      <br />
     </div>
   );
 }
