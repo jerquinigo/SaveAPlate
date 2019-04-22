6e85eff094ca4e148b919a674925f87088d8fcf8
Andrew KIl <kil.andrew@gmail.com>
2019-04-22 11:47:30 -0400

    Initial CSS for vendor profile vendor name, add item button, number of people fed, donation list

--- frontend/src/components/profiles/vendorProfiles/VendorProfile.js
+++ frontend/src/components/profiles/vendorProfiles/VendorProfile.js
@@ -1,8 +1,8 @@
-
 import React, { Component } from "react";
 import axios from "axios";
 import { getFoodItemsByVendor } from "../../../utils/UtilFoodItems.js";
 import AddItemForm from "./AddItemsForm.js";
+import "./vendorProfilesCSS/vendorProfile.css";
 
 class VendorProfile extends Component {
   constructor() {
@@ -15,19 +15,21 @@ class VendorProfile extends Component {
       claimedItems: [],
       unclaimedItems: []
     };
-
   }
 
   componentDidMount() {
     this.vendorDonations();
   }
 
-  ////////////////////////////////////////////////////////////// add food items /////////////////////////////////////////////////////////////////////
-
+  // Add food items
   addItemButton = () => {
-    return <button onClick={this.toAddItem}> Add Item </button>;
+    return (
+      <button onClick={this.toAddItem} className="add-item-button">
+        {" "}
+        Add Item{" "}
+      </button>
+    );
   };
-
 
   toAddItem = () => {
     this.setState({
@@ -35,13 +37,11 @@ class VendorProfile extends Component {
     });
   };
 
-
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
   };
-
 
   submitItem = e => {
     e.preventDefault();
@@ -58,40 +58,63 @@ class VendorProfile extends Component {
       });
   };
 
-
-  /////////////////////////////////////////////////////////////////Get items/////////////////////////////////////////////////////////////
+  // Get items
   vendorDonations = () => {
     let tempVar;
-    if(this.props.currentUser.type === 2){
-      tempVar = this.props.match.params.vendor
+    if (this.props.currentUser.type === 2) {
+      tempVar = this.props.match.params.vendor;
     }
     console.log(tempVar);
-    getFoodItemsByVendor(!tempVar ? this.props.currentUser.name : tempVar).then(data => {
+    getFoodItemsByVendor(!tempVar ? this.props.currentUser.name : tempVar).then(
+      data => {
+        let unclaimed = data.data.food_items.filter(item => {
+          return item.is_claimed === false;
+        });
 
-      let unclaimed = data.data.food_items.filter(item => {
-        return item.is_claimed === false;
-      });
-
-      this.setState({
-        unclaimedItems: unclaimed
-      });
-      let claimed = data.data.food_items.filter(item => {
-        return item.is_claimed === true;
-      });
-      this.setState({
-        claimedItems: claimed
-      });
-    });
+        this.setState({
+          unclaimedItems: unclaimed
+        });
+        let claimed = data.data.food_items.filter(item => {
+          return item.is_claimed === true;
+        });
+        this.setState({
+          claimedItems: claimed
+        });
+      }
+    );
   };
 
-  /////////////////////////////////////////////////////////////DISPLAY ITEMS ///////////////////////////////////////////////////////////////////
+  // Display items
   displayUnclaimedItems = () => {
-    return this.state.unclaimedItems.map((item, key) => {
-
-      let converted_time = Number(item.set_time.slice(0,2))
+    return this.state.unclaimedItems.map(item => {
+      let converted_time = Number(item.set_time.slice(0, 2));
 
       return (
-        <div key={item.food_id}>
+        <div key={item.food_id} id="display-unclaimed-items">
+          <p>{item.name}</p>
+          <p>{item.quantity} pounds</p>
+          <p>Feeds {item.quantity * 3} people</p>
+          <p>
+            {" "}
+            Lastest Pick Up Time{" "}
+            {converted_time === 0 || converted_time < 13
+              ? converted_time + "am"
+              : converted_time - 12 + "pm"}
+          </p>
+
+          {item.is_claimed ? (
+            <button
+              onClick={e => this.claimItem(e, item.is_claimed)}
+              id={item.food_id}>
+              CLAIMED
+            </button>
+          ) : (
+            <button
+              onClick={e => this.claimItem(e, item.is_claimed)}
+              id={item.food_id}>
+              UNCLAIMED
+            </button>
+          )}
           <button onClick={this.deleteItem} id={item.food_id}>
             <img
               id={item.food_id}
@@ -101,28 +124,6 @@ class VendorProfile extends Component {
               width="25"
             />
           </button>
-          <h2> Food Dish </h2>
-          <h3>{item.name}</h3>
-          <h4> Feeds </h4>
-          <h5>{item.quantity}</h5>
-          <h4> Lastest Pick Up Time </h4>
-
-          <h5>{converted_time === 0 || converted_time < 13 ? converted_time + "am" : converted_time-12 + "pm"}</h5>
-          {item.is_claimed ? (
-            <button
-              onClick={e => this.claimItem(e, item.is_claimed)}
-              id={item.food_id}
-            >
-              CLAIMED
-            </button>
-          ) : (
-            <button
-              onClick={e => this.claimItem(e, item.is_claimed)}
-              id={item.food_id}
-            >
-              UNCLAIMED
-            </button>
-          )}
         </div>
       );
     });
@@ -130,87 +131,103 @@ class VendorProfile extends Component {
 
   displayClaimedItems = () => {
     return this.state.claimedItems.map((item, key) => {
-      let converted_time = Number(item.set_time.slice(0,2))
+      let converted_time = Number(item.set_time.slice(0, 2));
 
       return (
-        <div key={item.food_id}>
-          <h3><strong> Food Dish </strong></h3>
+        <div key={item.food_id} id="">
+          <h3>
+            <strong> Food Dish </strong>
+          </h3>
           <h4>{item.name}</h4>
-          <h3><strong> Feeds </strong></h3>
+          <h3>
+            <strong> Feeds </strong>
+          </h3>
           <h4>{item.quantity}</h4>
-          <h3><strong> Pick Up Time </strong></h3>
-          <h4>{converted_time === 0 || converted_time < 13 ? converted_time + "am" : converted_time-12 + "pm"}</h4>
+          <h3>
+            <strong> Pick Up Time </strong>
+          </h3>
+          <h4>
+            {converted_time === 0 || converted_time < 13
+              ? converted_time + "am"
+              : converted_time - 12 + "pm"}
+          </h4>
           {item.is_claimed ? (
             <button
               onClick={e => this.claimItem(e, item.is_claimed)}
-              id={item.food_id}
-            >
+              id={item.food_id}>
               CLAIMED
             </button>
           ) : (
             <button
               onClick={e => this.claimItem(e, item.is_claimed)}
-              id={item.food_id}
-            >
+              id={item.food_id}>
               UNCLAIMED
             </button>
           )}
-
         </div>
       );
     });
   };
 
-  //////////////////////////////////////////to claim on vendor page////////////////////////////////////////////////////
+  // To claim on vendor page
   claimItem = (e, isClaimed) => {
-
-    if (this.props.currentUser.type){
-    axios.patch(`/api/fooditems/claimstatus/${e.target.id}`, {
-      client_id: this.props.currentUser.id,
-      is_claimed: !isClaimed
-    })
-      .then(() => {
-        this.vendorDonations()
-      })
+    if (this.props.currentUser.type) {
+      axios
+        .patch(`/api/fooditems/claimstatus/${e.target.id}`, {
+          client_id: this.props.currentUser.id,
+          is_claimed: !isClaimed
+        })
+        .then(() => {
+          this.vendorDonations();
+        });
     }
-  }
+  };
 
-//////////////////////////////////////////////DELETE ITEMS/////////////////////////////////////////////////////////
-  deleteItem = (e) => {
-    axios.delete(`/api/fooditems/${e.target.id}`)
-      .then(() => {
-        this.vendorDonations()
-      })
-  }
-//////////////////////////////////////FAVORITE VENDOR /////////////////////////////////////////////////////////////
+  // Delete items
+  deleteItem = e => {
+    axios.delete(`/api/fooditems/${e.target.id}`).then(() => {
+      this.vendorDonations();
+    });
+  };
 
-
+  // Favorite vendor
   render() {
     let vendorUser;
-    if(this.props.currentUser.type === 2){
-      vendorUser = this.props.match.params.vendor
+    if (this.props.currentUser.type === 2) {
+      vendorUser = this.props.match.params.vendor;
     }
-    console.log(this.state)
+    console.log(this.state);
     return (
-      <>
-        <div className="VendorProfileWrapper profile">
-
-          <h3> {!vendorUser ? this.props.currentUser.name: vendorUser} </h3>
-          {this.state.toAddItem ? (
-            <AddItemForm
-              handleChange={this.handleChange}
-              submitItem={this.submitItem}
-            />
-          ) : (
-            this.addItemButton()
-          )}
-          <h1><strong><i> Donation List </i></strong> </h1>
-          {this.displayUnclaimedItems()}
-          <h1><strong><i> Claimed Items </i></strong></h1>
+      <div className="vendor-profile-container">
+        <h1 className="vendor-name">
+          {" "}
+          {!vendorUser ? this.props.currentUser.name : vendorUser}{" "}
+        </h1>
+        <br />
+        {this.state.toAddItem ? (
+          <AddItemForm
+            handleChange={this.handleChange}
+            submitItem={this.submitItem}
+          />
+        ) : (
+          this.addItemButton()
+        )}
+        <div id="vendor-people-fed">
+          Number of people fed:
+          <p>0</p>
+        </div>
+        <br />
+        <div>
+          <h1 id="donation-list">Donation List</h1>
+          <div id="display-unclaimed-items-container">
+            {this.displayUnclaimedItems()}
+          </div>
+        </div>
+        <div>
+          <h1>Claimed Items</h1>
           {this.displayClaimedItems()}
         </div>
-
-      </>
+      </div>
     );
   }
 }
