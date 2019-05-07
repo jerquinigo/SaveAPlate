cc1fd2f91d7b0335f5f52b7711ddffc89e624250
Andrew KIl <kil.andrew@gmail.com>
2019-05-06 23:32:12 -0400

    profile pic

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,24 +2,20 @@ var express = require("express");
 var router = express.Router();
 
 const {
-getAllUsers,
-getAllVendors,
-getAllClients,
-updateUser,
-deleteUser,
-getSingleUser,
+  getAllUsers,
+  getAllVendors,
+  getAllClients,
+  updateUser,
+  deleteUser,
+  getSingleUser
 } = require("../db/queries/usersQueries.js");
 
+router.get("/", getAllUsers);
+router.get("/vendors", getAllVendors);
+router.get("/clients", getAllClients);
+router.get("/:id", getSingleUser);
+router.post("/new", createUser);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
-
-
-router.get('/', getAllUsers)
-router.get('/vendors', getAllVendors)
-router.get('/clients', getAllClients)
-router.get('/:id', getSingleUser)
-router.post('/new', createUser)
-router.patch('/:id', updateUser)
-router.delete('/:id', deleteUser)
-
-
-module.exports = router
+module.exports = router;
