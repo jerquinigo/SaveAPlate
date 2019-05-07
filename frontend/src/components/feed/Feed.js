3a118332358f50d818af7bcd37f71fae824961c1
Deyvi <deyvio@gmail.com>
2019-05-07 14:53:23 -0400

    search results are claimable

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -3,15 +3,15 @@ import "./feedCSS/Feed.css";
 import axios from "axios";
 import SearchBar from "./SearchBar.js";
 import AllFeedItems from "./AllFeedItems.js";
-import SearchBarResults from "./SearchBarResults.js";
+import { SearchBarResults } from "./SearchBarResults.js";
 import "./feedCSS/Feed.css";
 import MainSnackbarContainer from "../../containers/MainSnackbarContainer.js";
 
 export default class Feed extends Component {
   state = {
     allFoodItems: [],
-    userSearchResults: [],
-    textInput: ""
+    textInput: "",
+    searchText: ""
   };
 
   componentDidMount() {
@@ -32,9 +32,7 @@ export default class Feed extends Component {
   };
 
   claimItem = (e, isClaimed) => {
-    debugger;
     if (isClaimed === false) {
-      debugger;
       axios
         .patch(`/api/fooditems/claimstatus/${e.target.id}`, {
           client_id: this.props.currentUser.id,
@@ -57,19 +55,38 @@ export default class Feed extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
-    let searchResult = this.state.allFoodItems.filter(item => {
-      let vendor = item.vendor_name.toLowerCase();
-      let food = item.name.toLowerCase();
-      let text = this.state.textInput.toLowerCase();
-      let claimed = item.is_claimed;
-
-      return (vendor.includes(text) && claimed !== true) || food.includes(text);
-    });
-
+    // let searchResult = this.state.allFoodItems.filter(item => {
+    //   let vendor = item.vendor_name.toLowerCase();
+    //   let food = item.name.toLowerCase();
+    //   let text = this.state.textInput.toLowerCase();
+    //   let claimed = item.is_claimed;
+    //
+    //   return (vendor.includes(text) && claimed !== true) || food.includes(text);
+    // });
+    // const searchResults = this.search(
+    //   this.state.allFoodItems,
+    //   this.state.textInput.toLowerCase()
+    // );
     await this.setState({
-      userSearchResults: searchResult,
+      searchText: this.state.textInput.toLowerCase(),
       textInput: ""
     });
+  };
+  search = (allFoodItems, searchText) => {
+    //if search text is blank, return empty arr for length
+    if (this.state.searchText === "") return [];
+
+    let searchResult = allFoodItems.filter(item => {
+      let vendor = item.vendor_name.toLowerCase();
+      let food = item.name.toLowerCase();
+      let claimed = item.is_claimed;
+
+      return (
+        (vendor.includes(searchText) && claimed !== true) ||
+        food.includes(searchText)
+      );
+    });
+    return searchResult;
   };
 
   handleChange = e => {
@@ -86,7 +103,10 @@ export default class Feed extends Component {
   };
 
   render() {
-    console.log(this.state.userSearchResults, "in the length");
+    const filteredFoodItems = this.search(
+      this.state.allFoodItems,
+      this.state.searchText
+    );
     return (
       <div className="feedWrapper">
         <MainSnackbarContainer />
@@ -99,12 +119,13 @@ export default class Feed extends Component {
           receivedOpenSnackbar={this.props.receivedOpenSnackbar}
         />
 
-        {this.state.userSearchResults.length > 0 ? (
+        {filteredFoodItems.length > 0 ? (
           <SearchBarResults
             claimItem={this.claimItem}
-            userSearchResults={this.state.userSearchResults}
+            userSearchResults={filteredFoodItems}
             currentUser={this.props.currentUser.type}
             getAllFoodItems={this.getAllFoodItems}
+            foodItems={this.state.getAllFoodItems}
             receivedOpenSnackbar={this.props.receivedOpenSnackbar}
           />
         ) : (
@@ -119,16 +140,3 @@ export default class Feed extends Component {
     );
   }
 }
-
-// {this.state.userSearchResults.length > 0 ? (
-//   <SearchBarResults
-//     claimItem={this.claimItem}
-//     userSearchResults={this.state.userSearchResults}
-//   />
-// ) : (
-// <AllFeedItems
-//   claimItem={this.claimItem}
-//   allFoodItems={this.state.allFoodItems}
-//   userSearchResults={this.state.userSearchResults}
-// />
-// )}
--- frontend/src/components/feed/SearchBarResults.js
+++ frontend/src/components/feed/SearchBarResults.js
@@ -4,109 +4,76 @@ import Button from "@material-ui/core/Button";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import green from "@material-ui/core/colors/green";
 
-class SearchBarResults extends Component {
-  // will need to apply the search object filter here just like all the others to get it to work to group
-  displaySearchResults = () => {
-    let searchDataObj = {};
-    let converted_time;
-    if (this.props.userSearchResults.length > 0) {
-      let searchResults = this.props.userSearchResults.filter(result => {
-        return result.is_claimed !== true;
-      });
-      console.log(searchResults, "searchResult");
-      searchResults.map((results, i) => {
-        if (!searchDataObj[results.vendor_name]) {
-          searchDataObj[results.vendor_name] = [results];
-        } else if (searchDataObj[results.vendor_name]) {
-          searchDataObj[results.vendor_name].push(results);
-          console.log(searchDataObj, "in the search results");
-        }
-        converted_time = Number(results.set_time.slice(0, 2));
-      });
-      let vendorNameArr = Object.keys(searchDataObj);
+// will need to apply the search object filter here just like all the others to get it to work to group
+export const SearchBarResults = props => {
+  let searchDataObj = {};
+  let converted_time;
+  if (props.userSearchResults.length > 0) {
+    let searchResults = props.userSearchResults.filter(result => {
+      return result.is_claimed !== true;
+    });
+    console.log(searchResults, "searchResult");
+    searchResults.map((results, i) => {
+      if (!searchDataObj[results.vendor_name]) {
+        searchDataObj[results.vendor_name] = [results];
+      } else if (searchDataObj[results.vendor_name]) {
+        searchDataObj[results.vendor_name].push(results);
+        console.log(searchDataObj, "in the search results");
+      }
+      converted_time = Number(results.set_time.slice(0, 2));
+    });
+    let vendorNameArr = Object.keys(searchDataObj);
 
-      let vendorName = vendorNameArr.map((vendorName, a) => {
-        return (
-          <div key="a">
-            <span>
-              <Link to={"/clientview/" + vendorName}>
-                <strong>{vendorName}</strong>{" "}
-              </Link>
-            </span>
+    let vendorName = vendorNameArr.map((vendorName, a) => {
+      return (
+        <div key="a">
+          <span>
+            <Link to={"/clientview/" + vendorName}>
+              <strong>{vendorName}</strong>{" "}
+            </Link>
+          </span>
 
-            <div className="vendorItemsWrapper">
-              {searchDataObj[vendorName].map((food, b) => {
-                debugger;
-                return (
-                  <div className="vendorItemsContainer" key={b}>
-                    <span>{food.address_field}</span>
-                    <span>{food.telephone_number}</span>
-                    <span>{food.name}</span>
-                    <span> Feeds: {food.quantity} people</span>
-                    <span>({Number(food.quantity) * 3} pounds)</span>
-                    <span>
-                      {converted_time === 0 || converted_time < 13
-                        ? converted_time + "am"
-                        : converted_time - 12 + "pm"}
-                    </span>
+          <div className="vendorItemsWrapper">
+            {searchDataObj[vendorName].map((food, b) => {
+              return (
+                <div className="vendorItemsContainer" key={b}>
+                  <span>{food.address_field}</span>
+                  <span>{food.telephone_number}</span>
+                  <span>{food.name}</span>
+                  <span> Feeds: {food.quantity} people</span>
+                  <span>({Number(food.quantity) * 3} pounds)</span>
+                  <span>
+                    {converted_time === 0 || converted_time < 13
+                      ? converted_time + "am"
+                      : converted_time - 12 + "pm"}
+                  </span>
 
-                    <span>
-                      <MuiThemeProvider theme={theme}>
-                        <Button
-                          id={food.id}
-                          variant="contained"
-                          color="secondary"
-                          onClick={e => {
-                            this.props.claimItem(e, food.is_claimed);
-                            this.props.receivedOpenSnackbar();
-                          }}
-                          className={
-                            food.is_claimed
-                              ? "claimed-button"
-                              : "unclaimed-button"
-                          }
-                        >
-                          {food.is_claimed ? "UNCLAIM" : "CLAIM"}
-                        </Button>
-                      </MuiThemeProvider>
-
-                      {/*{food.is_claimed ? (
-                        <button
-                          className={food.is_claimed ? "unclaimed" : "claimed"}
-                          onClick={e =>
-                            this.props.claimItem(e, food.is_claimed)
-                          }
-                          id={food.id}
-                        >
-                          UNCLAIM
-                        </button>
-                      ) : (
-                        <button
-                          className={food.is_claimed ? "unclaimed" : "claimed"}
-                          onClick={e =>
-                            this.props.claimItem(e, food.is_claimed)
-                          }
-                          id={food.id}
-                        >
-                          CLAIM
-                        </button>
-                      )}*/}
-                    </span>
-                  </div>
-                );
-              })}
-            </div>
+                  <MuiThemeProvider theme={theme}>
+                    <Button
+                      id={food.id}
+                      variant="contained"
+                      color="secondary"
+                      onClick={e => {
+                        props.claimItem(e, food.is_claimed);
+                        props.receivedOpenSnackbar();
+                      }}
+                      className={
+                        food.is_claimed ? "claimed-button" : "unclaimed-button"
+                      }
+                    >
+                      {food.is_claimed ? "UNCLAIM" : "CLAIM"}
+                    </Button>
+                  </MuiThemeProvider>
+                </div>
+              );
+            })}
           </div>
-        );
-      });
-      return vendorName;
-    }
-  };
-
-  render() {
-    return <div>{this.displaySearchResults()}</div>;
+        </div>
+      );
+    });
+    return vendorName;
   }
-}
+};
 
 const theme = createMuiTheme({
   palette: {
@@ -119,5 +86,3 @@ const theme = createMuiTheme({
     useNextVariants: true
   }
 });
-
-export default SearchBarResults;
