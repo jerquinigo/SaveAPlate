972a8b869c950d95b2e7cde1690a8a4653eb3f93
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-05-10 17:11:50 -0400

    fixed issues and organized similar css

--- frontend/src/components/feed/AllFeedItemsDisplayed.js
+++ frontend/src/components/feed/AllFeedItemsDisplayed.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import green from "@material-ui/core/colors/green";
+import "./feedCSS/AllFeedItemsDisplayed.css";
 
 class AllFeedItemsDisplayed extends Component {
   render() {
@@ -10,7 +11,7 @@ class AllFeedItemsDisplayed extends Component {
         {this.props.foodDataObj[this.props.vendorName].map((food, b) => {
           return (
             <div className="vendorItemsContainer" key={b}>
-              <div className="display-claimed-items">
+              <div className="display-claimed-items-for-client">
                 <div className="display-food-name">{food.name}</div>
 
                 <span> Feeds: {food.quantity} people</span>
@@ -26,14 +27,16 @@ class AllFeedItemsDisplayed extends Component {
                   onClick={e => {
                     this.props.claimItem(e, food.is_claimed);
                     this.props.receivedOpenSnackbar();
-                  }}>
+                  }}
+                >
                   <MuiThemeProvider theme={theme}>
                     <Button
                       variant="contained"
                       color="secondary"
                       className={
                         food.is_claimed ? "claimed-button" : "unclaimed-button"
-                      }>
+                      }
+                    >
                       {food.is_claimed ? "UNCLAIM" : "CLAIM"}
                     </Button>
                   </MuiThemeProvider>
