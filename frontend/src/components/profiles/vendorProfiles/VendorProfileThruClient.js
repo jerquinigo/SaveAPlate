63ad829cc2fb18d54ee8751f153f9934c5aa4fac
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-05-17 00:18:44 -0400

    fixed vendorprofilethroughtclient misalign text

--- frontend/src/components/profiles/vendorProfiles/VendorProfileThruClient.js
+++ frontend/src/components/profiles/vendorProfiles/VendorProfileThruClient.js
@@ -147,7 +147,8 @@ class VendorProfileThruClient extends Component {
             this.state.fadeTrigger.includes(item.food_id)
               ? "vendor-profile-container-vendor-version fade-out"
               : "vendor-profile-container-vendor-version"
-          }>
+          }
+        >
           <div className="claimed-vendor-items-two">
             <p className="vendor-page-item-name">{item.name}</p>
             <p className="vendor-page-item-pounds">
@@ -170,7 +171,8 @@ class VendorProfileThruClient extends Component {
                 color="secondary"
                 className={
                   item.is_claimed ? "claimed-button " : "unclaimed-button "
-                }>
+                }
+              >
                 {item.is_claimed ? "UNCLAIM" : "CLAIM"}
               </Button>
             </MuiThemeProvider>
@@ -184,10 +186,16 @@ class VendorProfileThruClient extends Component {
           <h3 className="donation-list-text"> Donation List </h3>
         </div>
         <div className="vendor-items-list-header-vendor-view-through-client">
-          <h4 className="vendor-profile-thru-client-item-name">Food Item </h4>
-          <h4 className="vendor-profile-thru-client-weight">Weight </h4>
-          <h4 className="vendor-profile-thru-client-feeds">Feeds</h4>
-          <h4 className="vendor-profile-thru-client-pick-up">Pick Up Time </h4>
+          <p className="vendor-profile-thru-client-item-name-for-client">
+            Food Item{" "}
+          </p>
+          <p className="vendor-profile-thru-client-weight-for-client">
+            Weight{" "}
+          </p>
+          <p className="vendor-profile-thru-client-feeds-for-client">Feeds</p>
+          <p className="vendor-profile-thru-client-pick-up-for-client">
+            Pick Up Time{" "}
+          </p>
           <div id="spacing" />
         </div>
         {unclaimedList}{" "}
@@ -204,7 +212,8 @@ class VendorProfileThruClient extends Component {
       return (
         <div
           key={item.food_id}
-          className="vendor-profile-container-vendor-version">
+          className="vendor-profile-container-vendor-version"
+        >
           <div className="claimed-vendor-items-two">
             <p className="vendor-page-item-name">{item.name}</p>
             <p className="vendor-page-item-pounds">
@@ -222,7 +231,8 @@ class VendorProfileThruClient extends Component {
                 id={item.food_id}
                 variant="contained"
                 color="primary"
-                onClick={e => this.claimItem(e, item.is_claimed, item.food_id)}>
+                onClick={e => this.claimItem(e, item.is_claimed, item.food_id)}
+              >
                 {item.is_claimed ? "UNCLAIM" : "CLAIM"}
               </Button>
             </MuiThemeProvider>
@@ -265,7 +275,8 @@ class VendorProfileThruClient extends Component {
       return (
         <div
           className="main-div-displaying-detail-vendor-view-through-profile"
-          key={i}>
+          key={i}
+        >
           {this.getProfilePicture()}
           <div className="vendorNameDiv">
             <h2 className="vendor-name">{time.name} </h2>
@@ -279,7 +290,8 @@ class VendorProfileThruClient extends Component {
                 !!this.state.isFav.length
                   ? "claimed-button"
                   : "unclaimed-button"
-              }>
+              }
+            >
               {!!this.state.isFav.length
                 ? "Remove From Favorites"
                 : "Add To Favorites"}
